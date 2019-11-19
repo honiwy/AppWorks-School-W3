795cc7c2203429e1d790da559543cc802e072673
WYC <honiwy@gmail.com>
2019-11-19 07:28:45 -0500

    Add LiveData Encapsulation to GameViewModel
    把score, word透過唯讀方式封裝起來 這樣GameFragment就不會改到數值

--- andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.android.guesstheword.screens.game
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import timber.log.Timber
@@ -7,10 +8,14 @@ import timber.log.Timber
 class GameViewModel:ViewModel(){
 
     // The current word
-    var word  = MutableLiveData<String>()
+    private val _word  = MutableLiveData<String>()
+val word:LiveData<String>
+    get()=_word
 
     // The current score
-    var score = MutableLiveData<Int>()
+    private val _score = MutableLiveData<Int>()
+    val score:LiveData<Int>
+           get()=_score
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
@@ -19,7 +24,7 @@ class GameViewModel:ViewModel(){
         //Log.i("GameViewModel",)
         Timber.i("GameViewModel created!")
         resetList()
-        score.value = 0
+        _score.value = 0
     }
 
     override fun onCleared() {
@@ -67,17 +72,17 @@ class GameViewModel:ViewModel(){
         if (wordList.isEmpty()) {
             //gameFinished()
         } else {
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
     }
     /** Methods for buttons presses **/
     fun onSkip() {
-        score.value = (score.value)?.minus(1)
+        _score.value = (score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score.value = (score.value)?.plus(1)
+        _score.value = (score.value)?.plus(1)
         nextWord()
     }
 
